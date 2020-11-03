95e387267e9f3fb6e6efec0635577858b4c04c35
Pavel Kriz <pavel.kriz@uhk.cz>
2020-11-03 12:53:15 +0100

    Initial commit

---
+++ src/cz/uhk/pro2/tcpchat/server/MessageBroacaster.java
@@ -0,0 +1,5 @@
+package cz.uhk.pro2.tcpchat.server;
+
+public interface MessageBroacaster {
+    void broadcastMessage(String message);
+}
---
+++ src/cz/uhk/pro2/tcpchat/server/TcpChatServer.java
@@ -0,0 +1,52 @@
+package cz.uhk.pro2.tcpchat.server;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TcpChatServer implements MessageBroacaster {
+    final List<Socket> connectedClients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        TcpChatServer s = new TcpChatServer();
+        s.start();
+    }
+
+    private void start() {
+        try {
+            ServerSocket socket = new ServerSocket(5000);
+            while (true) {
+                Socket connectedClient = socket.accept();
+                System.out.println("New client connected " + connectedClient);
+                synchronized (connectedClients) {
+                    connectedClients.add(connectedClient);
+                }
+                TcpServerUserThread t = new TcpServerUserThread(connectedClient, this);
+                t.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void broadcastMessage(String message) {
+        // TODO DU 3.11.2020 neposilat zpravu tomu, kdo ji odelal (puvodci)
+        synchronized (connectedClients) {
+            for (Socket s : connectedClients) {
+                try {
+                    OutputStream os = s.getOutputStream();
+                    PrintWriter w = new PrintWriter(new OutputStreamWriter(os), true);
+                    w.println(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
